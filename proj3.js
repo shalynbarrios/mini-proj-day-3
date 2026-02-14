5d8b4973458f46a8ba3df3b5037e1a5e0c578925
Shalyn Barrios <165353062+shalynbarrios@users.noreply.github.com>
2026-02-14 10:32:04 -0600

    painter project begun

---
+++ proj3.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(1300, 1300);
+    background(100);
+}
+
+function draw() {
+  // below
+  noStroke(0);
+  fill(200, 0, 100)
+  ellipse(mouseX, mouseY, 75, 120)
+}
